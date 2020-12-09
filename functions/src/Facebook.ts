f8a5f0211819ded419aacfc5a792f09f4127acfd
James Dearlove <jamesdearlove@outlook.com>
2020-12-09 23:59:33 +1000

    Fixup Facebook functions

--- functions/src/Facebook.ts
+++ functions/src/Facebook.ts
@@ -53,14 +53,11 @@ export const userFacebookLogin = functions.https.onCall(
         );
       }
 
-      await admin
-        .firestore()
-        .doc(`users/${context.auth?.uid}`)
-        .set({
-          facebookUserName: userDataRequest.data.name,
-          facebookUserID: userDataRequest.data.id,
-          facebookUserToken: tokenRequest.data.access_token,
-        });
+      await admin.firestore().doc(`users/${context.auth?.uid}`).set({
+        facebookUserName: userDataRequest.data.name,
+        facebookUserID: userDataRequest.data.id,
+        facebookUserToken: tokenRequest.data.access_token,
+      });
     }
 
     return tokenRequest.data.access_token;
@@ -74,82 +71,85 @@ export const userFacebookLogout = functions.https.onCall((data, context) => {
 // TODO: Graceful handling of errors
 export const getUserFacebookPages = functions.https.onCall(
   async (data, context) => {
-  const userData = await admin.firestore().doc(`users/${context.auth?.uid}`).get()
-  const userToken = userData.data()?.facebookUserToken;
+    const userData = await admin
+      .firestore()
+      .doc(`users/${context.auth?.uid}`)
+      .get();
+    const userToken = userData.data()?.facebookUserToken;
 
-  const pageListRequest = await instance.get("me/accounts", {
-    params: {
-      access_token: userToken,
-      fields: "id,name",
-      // fields: "access_token,id,name",
-    }
-  })
+    const pageListRequest = await instance.get("me/accounts", {
+      params: {
+        access_token: userToken,
+        fields: "id,name",
+        // fields: "access_token,id,name",
+      },
+    });
 
-  // if (!pageListRequest.data.error) {
-  //   throw new functions.https.HttpsError(
-  //     "internal",
-  //     "An unkown error occurred with the Facebook API."
-  //   );
-  // }
+    // if (!pageListRequest.data.error) {
+    //   throw new functions.https.HttpsError(
+    //     "internal",
+    //     "An unkown error occurred with the Facebook API."
+    //   );
+    // }
 
-  return pageListRequest.data.data;
-  
-});
+    return pageListRequest.data.data;
+  }
+);
 
 // TODO: Graceful handling of errors
 export const setUserFacebookPage = functions.https.onCall(
   async (data, context) => {
+    const pageID = data.pageID;
 
-  const pageID = data.pageID;
+    const userData = await admin
+      .firestore()
+      .doc(`users/${context.auth?.uid}`)
+      .get();
+    const userToken = userData.data()?.facebookUserToken;
 
-  const userData = await admin.firestore().doc(`users/${context.auth?.uid}`).get()
-  const userToken = userData.data()?.facebookUserToken;
-
-  const tokenRequest = await instance.get(`${pageID}/`, {
-    params: {
-      access_token: userToken,
-      fields: "access_token",
-    }
-  })
-
-  if (!tokenRequest.data.access_token) {
-    if (!tokenRequest.data.error) {
-      throw new functions.https.HttpsError(
-        "internal",
-        "An unkown error occurred with the Facebook API."
-      );
-    } else {
-      throw new functions.https.HttpsError(
-        "permission-denied",
-        `Recieved error when retrieving page token. ${tokenRequest.data.error.code}: ${tokenRequest.data.error.message}`
-      );
-    }
-  } else {
-    const userDataRequest = await instance.get("me", {
+    const tokenRequest = await instance.get(`${pageID}/`, {
       params: {
-        access_token: tokenRequest.data.access_token,
+        access_token: userToken,
+        fields: "access_token",
       },
     });
 
-    if (!userDataRequest || !userDataRequest.data.name) {
-      throw new functions.https.HttpsError(
-        "internal",
-        "The page token given by Facebook was not valid."
-      );
-    }
+    if (!tokenRequest.data.access_token) {
+      if (!tokenRequest.data.error) {
+        throw new functions.https.HttpsError(
+          "internal",
+          "An unkown error occurred with the Facebook API."
+        );
+      } else {
+        throw new functions.https.HttpsError(
+          "permission-denied",
+          `Recieved error when retrieving page token. ${tokenRequest.data.error.code}: ${tokenRequest.data.error.message}`
+        );
+      }
+    } else {
+      const userDataRequest = await instance.get("me", {
+        params: {
+          access_token: tokenRequest.data.access_token,
+        },
+      });
 
-    await admin
-      .firestore()
-      .doc(`users/${context.auth?.uid}`)
-      .update({
+      if (!userDataRequest || !userDataRequest.data.name) {
+        throw new functions.https.HttpsError(
+          "internal",
+          "The page token given by Facebook was not valid."
+        );
+      }
+
+      await admin.firestore().doc(`users/${context.auth?.uid}`).update({
         facebookPageName: userDataRequest.data.name,
         facebookPageID: userDataRequest.data.id,
         facebookPageToken: tokenRequest.data.access_token,
       });
-  }
+    }
 
-  return "Success"
-});
+    return "Success";
+  }
+);
 
 export const publishFacebookPost = functions.https.onCall(
   async (data, context) => {
@@ -168,7 +168,7 @@ export const publishFacebookPost = functions.https.onCall(
       fields: "permalink_url",
     });
 
-    if (creationResult.status == 200) {
+    if (creationResult.status === 200) {
       await postDataRef.update({ postedOn: new Date() });
     }
 
